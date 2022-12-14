3f5e0a143a7d23658e98b0812df3dcb8c62df065
glnsb <marcusorhan@gmail.com>
2022-12-13 23:26:08 -0500

    object and class

--- 05-objects.js
+++ 05-objects.js
@@ -1,7 +1,27 @@
 const cookie = {
    name: "Chockolotos chipos",
    isGlutenFree: false,
-   isPackaged:true
+   isPackaged:true,
+   "+Yummy" :true,
+   eatCookie : function (){
+console.log("I am eating " +this.name+  "  HMMM its yummy "+this["+Yummy"]);
+   }
 };
-
-console.log(cookie);
+cookie.name = "Tarantula gingerbread";
+cookie.isPackaged=false;
+// console.log(cookie["+Yummy"]);
+// console.log(cookie);
+cookie.eatCookie();
+class Cookie {
+    constructor (name, isGlutenFree, isPackaged, ) {
+        this.name = name;
+        this.isGlutenFree = isGlutenFree;
+        this.isPackaged = isPackaged;
+    }
+eatCookie( ){
+console.log("I was eating " + this.name + " cookie in The CLASS." )
+}
+}
+const myCookie = new Cookie("Oreo", false, false);
+console.log(myCookie);
+myCookie.eatCookie();
