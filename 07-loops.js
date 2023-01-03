aa4508f99f4a046882310ceb60f114b2bc935b09
glnsb <marcusorhan@gmail.com>
2023-01-02 20:55:47 -0500

    main

--- 07-loops.js
+++ 07-loops.js
@@ -8,3 +8,10 @@ const numbers = [ 1, 2,5, 7, 13, 17, 42 ];
 for(let j = numbers.length; j>=0; j--){
     console.log(numbers[j]);
 }
+
+for(let i=0; i<5; i++){
+    for(let j=0; j<i; j++){
+        console.log("* ");
+    }j=0;
+}console.log("About RanksTAU 100Learning PathsCertificatesTAU Slack Marcus M Orhan Logout ");
+   
